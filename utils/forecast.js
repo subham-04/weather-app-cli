95a48987e6ee333d69e055d6cbc10390bd9e276f
subham-04 <subham0422@gmail.com>
2023-03-11 21:08:01 +0530

    2nd version

--- utils/forecast.js
+++ utils/forecast.js
@@ -3,15 +3,15 @@ const request = require('request')
 const forecast = (latitude, longtitude, callback)=>{
     const url = 'https://api.openweathermap.org/data/2.5/weather?lat='+latitude+'&lon='+longtitude+'&appid=181dec9ca1598c9b982c98a5e3da6606&units=metric';
 
-    request({url: url, json:true}, (error,response)=>{
+    request({url: url, json:true}, (error, { body })=>{
         
         
     if(error){ //this error checking is done at lower level (with the os)
         callback(`Unable to connect to weather services.`,undefined);
-    }else if(response.body.message){ //this error is from the API
+    }else if(body.message){ //this error is from the API
         callback(`Unable to find location.`,undefined);
     }else{
-        const data = response.body;
+        const data = body;
         const temp= data.main.temp;
         const tempLoc= 'Temperature of '+data.name+' is '+temp+'°C';
         callback(undefined, tempLoc);
--- utils/geocode.js
+++ utils/geocode.js
@@ -4,13 +4,13 @@ const request = require("request");
 const geocode = (address, callback)=>{
     const url = 'https://us1.locationiq.com/v1/search?key=pk.0df5c8b35f84cface9b5e803be61f596&q='+encodeURIComponent(address)+'&format=json&limit=1';
 
-    request({url: url, json:true}, (error,response)=>{
+    request({url: url, json:true}, (error,{ body })=>{
         if(error){
             callback(`Unable to connect to the location services`, undefined);
-        }else if(response.body.error){
+        }else if(body.error){
             callback(`Unable to find location`, undefined);
         }else{
-            const data = response.body[0];
+            const data = body[0];
             callback(undefined,{
                 latitude: data.lat,
                 longtitude: data.lon,
